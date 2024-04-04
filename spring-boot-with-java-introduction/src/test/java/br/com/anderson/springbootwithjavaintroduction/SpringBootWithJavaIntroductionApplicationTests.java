4d5f0f5a1f3e2f6d639ef1af35fc41cd69bac763
Anderson Silva <andersonline62@gmail.com>
2024-04-04 09:21:29 -0300

    initial commit

---
+++ spring-boot-with-java-introduction/src/test/java/br/com/anderson/springbootwithjavaintroduction/SpringBootWithJavaIntroductionApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.anderson.springbootwithjavaintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJavaIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
